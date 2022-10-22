140935e50ee814a1f841dbb703da5bac4efff1fa
Santiago <santicardonas777@gmail.com>
2022-10-21 23:55:47 -0500

    counter component

---
+++ 03-fundamentos/src/components/Counter.vue
@@ -0,0 +1,32 @@
+<template>
+    <h2>Counter!!!!</h2>
+    <p>{{this.counter}} <sup>2</sup> = {{squereCounter}}</p>
+
+
+</template>
+
+<script>
+export default {
+
+    data(){
+        return{
+            counter: 5
+        }
+    },
+    methods: {
+        getSquareValue(){
+            return this.counter * this.counter
+        }
+    },
+    computed: {
+        squereCounter(){
+            return this.counter * this.counter
+        }
+    }
+
+}
+</script>
+
+<style>
+
+</style>
